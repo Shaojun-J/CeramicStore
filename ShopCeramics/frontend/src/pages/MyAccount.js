9e0b46a381deee6e962f6c767880dfec908ac192
Jennifer <100975749+Jennifer328@users.noreply.github.com>
2023-12-22 18:11:20 -0500

    add OrdersTable

--- ShopCeramics/frontend/src/pages/MyAccount.js
+++ ShopCeramics/frontend/src/pages/MyAccount.js
@@ -7,9 +7,10 @@ import {useLogout} from '../hooks/useLogout';
 
 import {getHours} from 'date-fns';
 
-import Orders from '../components/Orders';
+import OrdersTable from '../components/Order/OrdersTable';
 import Navbar from '../components/Navbar';
 import Promo from '../components/Promo';
+import Footer from '../components/Footer/Footer';
 
 
 const MyAccount = () => {
@@ -18,6 +19,90 @@ const MyAccount = () => {
     const {user} = useAuthContext();
     const{logout} = useLogout();
  
+   const orders = [  //THIS IS ONLY FOR TEST.we will pass the real data after the orders collection being setup in mongodb.
+    {
+      "id": 5641,
+      "orderDate": "2023-07-02",
+      "orderNumber": 26260,
+      "totalPrice": 394.4,
+      "totalItems": 10,
+      "status": "Shipped"
+    },
+    {
+      "id": 2178,
+      "orderDate": "2022-01-30",
+      "orderNumber": 43930,
+      "totalPrice": 238.25,
+      "totalItems": 3,
+      "status": "Pending"
+    },
+    {
+      "id": 8064,
+      "orderDate": "2022-05-28",
+      "orderNumber": 44489,
+      "totalPrice": 439.95,
+      "totalItems": 3,
+      "status": "Shipped"
+    },
+    {
+      "id": 7507,
+      "orderDate": "2023-09-19",
+      "orderNumber": 27223,
+      "totalPrice": 460.33,
+      "totalItems": 5,
+      "status": "Shipped"
+    },
+    {
+      "id": 1086,
+      "orderDate": "2022-05-22",
+      "orderNumber": 53901,
+      "totalPrice": 186.39,
+      "totalItems": 2,
+      "status": "Pending"
+    },
+    {
+      "id": 3050,
+      "orderDate": "2023-05-22",
+      "orderNumber": 81329,
+      "totalPrice": 274.54,
+      "totalItems": 10,
+      "status": "Pending"
+    },
+    {
+      "id": 5111,
+      "orderDate": "2022-08-17",
+      "orderNumber": 37005,
+      "totalPrice": 464.79,
+      "totalItems": 8,
+      "status": "Delivered"
+    },
+    {
+      "id": 3699,
+      "orderDate": "2022-09-08",
+      "orderNumber": 16370,
+      "totalPrice": 256.14,
+      "totalItems": 10,
+      "status": "Delivered"
+    },
+    {
+      "id": 2339,
+      "orderDate": "2022-06-26",
+      "orderNumber": 81758,
+      "totalPrice": 79.78,
+      "totalItems": 5,
+      "status": "Shipped"
+    },
+    {
+      "id": 2591,
+      "orderDate": "2022-09-19",
+      "orderNumber": 68455,
+      "totalPrice": 332.09,
+      "totalItems": 6,
+      "status": "Shipped"
+    }
+  ]
+  
+   
    
     useEffect(()=>{
       const currentTime = new Date();
@@ -43,17 +128,19 @@ const MyAccount = () => {
     }
 
   return (
-    <div>
+    <div className='page-container'>
        <Promo/>
        <Navbar/>
        {user && 
-         <div className='page-container'>
+         <div className='page-content'>
             <h2>Good {timeOfDay}, {(user.username || user.email.substring(0, user.email.indexOf('@'))).toUpperCase()}!</h2>
             <h5 className='page-subtitle'>Your Order History</h5>
-            <Orders/>
+            <OrdersTable data={orders}/>
             <button className='btn' onClick={handleLogout}>Logout</button>
          </div>  
+         
        }
+       <Footer/>
      </div> 
   )
 }
